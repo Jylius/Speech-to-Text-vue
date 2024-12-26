294601d245351cd1ba8a36923db11f87ab5004ac
Jylius <orkun88.tokyo@gmail.com>
2024-12-26 22:09:42 +0300

    :)

---
+++ components/noteItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="note_item">
+      <div class="id_content">
+        <span>#{{ noteItem.id }}</span>
+      </div>
+      <div class="note_content">
+        {{ noteItem.note }}
+      </div>
+    </div>
+    
+  </div>
+</template>
+<script>
+export default {
+  props:{
+    noteItem:{
+      type:Object,
+      required:true
+    }
+  }
+  
+}
+</script>
+<style>
+  
+</style>
---
+++ src/components/appHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <header>
+      Speech Recognition | Vue.js | orkun baba
+    </header>
+  </template>
